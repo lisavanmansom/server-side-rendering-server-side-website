88749a83261426a000c91f086d1f1fc17f10653f
lisavanmansom <lisavanmansom@gmail.com>
2024-03-06 13:58:58 +0100

    server side

--- server.js
+++ server.js
@@ -29,38 +29,27 @@ app.use(express.urlencoded({ extended: true }));
 
 
 /*** Routes & data ***/
-
-// 2. Routes die HTTP Request and Responses afhandelen
-
-// Haal alle squads uit de WHOIS API op
 const storyData = await fetchJson(apiUrl + '/tm_story')
-// const languageData = await fetchJson(apiUrl + '/tm_language')
-// const speakerData = await fetchJson(apiUrl + '/tm_speaker_profile')
-// const audioData = await fetchJson(apiUrl + '/tm_audio')
-// const playlistData = await fetchJson(apiUrl + '/tm_playlist')
-
 
 // Maak een GET route voor de index
 // Stap 1
-app.get('/', function (request, response) {
-  // Stap 2
-  // Haal alle personen uit de WHOIS API op
-  fetchJson(apiUrl + '/tm_story').then((apiData) => {
-    // apiData bevat gegevens van alle personen uit alle squads
-    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
-    // Stap 3
-    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+app.get('/', function(request, response) {
+  response.render('index')
+})
 
-    // Stap 4
-    // HTML maken op basis van JSON data
-    response.render('index', {
-      // persons: persons.data,
-      stories: storyData.data})
-  })
+app.get('/lessons', function(request, response) {
+  fetchJson('https://fdnd-agency.directus.app/items/tm_story').then((storyData) => {
+    response.render('lessons', {stories: storyData.data})
+  });
+})
+
+app.get('/lessons/:id', function(request, response){
+  fetchJson('https://fdnd-agency.directus.app/items/tm_story?filter={"id":' + request.params.id + '}').then((storyData) => {
+    response.render('lessons', {stories: storyData.data})
+  });
 })
 
 // 3. Start de webserver
-
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
 
